1f754328fb4f572fe35e933ec8aba145e100fb9e
Smile Studio <smilingky@gmail.com>
2014-11-06 21:57:33 +0100

    Add Umeng basic support Remove unnecessary resource files

--- WizardEscape-android/src/com/smilestudio/wizardescape/MainActivity.java
+++ WizardEscape-android/src/com/smilestudio/wizardescape/MainActivity.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.umeng.analytics.game.UMGameAgent;
 
 public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //init Umeng
+        UMGameAgent.setDebugMode(true);
+        UMGameAgent.init( this );
+
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useGL20 = true;
-
         initialize(new WizardEscape(), cfg);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        UMGameAgent.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        UMGameAgent.onPause(this);
+    }
+
 }
